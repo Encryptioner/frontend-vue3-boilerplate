0460ac30042f6a56d97f2815cf6bf32688f7d5d2
Encryptioner <mir.ankur.ruet13@gmail.com>
2022-06-23 09:41:40 +0600

    testing reactivity

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,18 @@
 <template>
   <h2 class="!mt-0 font-poppins">{{ props.msg }}</h2>
-  <button class="rounded-md border border-gray-300 bg-white px-3 py-2 shadow" @click="increment">
+  <button
+    :class="`rounded-${count ? 'full' : 'md'} border border-gray-300 bg-white px-3 py-2 shadow`"
+    @click="increment"
+  >
     count is: {{ count }}
   </button>
+  <button
+    :class="`${finalRoundedClass} ml-2 rounded-full border border-gray-300 bg-white px-3 py-2 shadow`"
+    @click="reset"
+  >
+    Reset
+  </button>
+
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
@@ -15,7 +25,13 @@ const props = defineProps<{
   optionalProp?: number;
 }>();
 
+const finalRoundedClass = ref<string>(`rounded-${count ? 'full' : 'md'}`);
+
 function increment() {
   mainStore.increment();
+}
+
+function reset() {
+  mainStore.$reset();
 }
 </script>
